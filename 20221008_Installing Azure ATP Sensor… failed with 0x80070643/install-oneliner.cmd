a71dd1608bc0359d3668b5c3fae6847eaa8930eb
Tomica Kaniski <tomica@outlook.com>
2022-10-17 12:22:58 +0200

    20221008 code added

---
+++ 20221008_Installing Azure ATP Sensor… failed with 0x80070643/install-oneliner.cmd
@@ -0,0 +1,2 @@
+# quietly installs Azure ATP (Microsoft Defender for Identity) Sensor, with Internet proxy specified
+"Azure ATP Sensor Setup.exe" /quiet ProxyUrl="http://proxy.corporate.local:8080" NetFrameworkCommandLineArguments="/q" AccessKey="<<< MY WORKSPACE ACCESS KEY GOES HERE >>>"
